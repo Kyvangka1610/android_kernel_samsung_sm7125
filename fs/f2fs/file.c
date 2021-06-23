15ec1dd1049681a505142254aa14f6a9eececa66
Jebaitedneko <Jebaitedneko@gmail.com>
2021-06-23 06:29:17 +0530

    fs: f2fs: backport ufs tw changes from 4.19
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -21,6 +21,10 @@
 #include <linux/uuid.h>
 #include <linux/file.h>
 
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+#include <linux/ufstw.h>
+#endif
+
 #include "f2fs.h"
 #include "node.h"
 #include "segment.h"
@@ -275,6 +279,10 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 	};
 	unsigned int seq_id = 0;
 
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+	bool turbo_set = false;
+#endif
+
 	if (unlikely(f2fs_readonly(inode->i_sb) ||
 				is_sbi_flag_set(sbi, SBI_CP_DISABLED)))
 		return 0;
@@ -351,6 +359,10 @@ go_write:
 		sbi->sec_stat.cp_cnt[STAT_CP_FSYNC]++;
 		goto out;
 	}
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+	bdev_set_turbo_write(sbi->sb->s_bdev);
+	turbo_set = true;
+#endif
 sync_nodes:
 	atomic_inc(&sbi->wb_sync_req[NODE]);
 	ret = f2fs_fsync_node_pages(sbi, inode, &wbc, atomic, &seq_id);
@@ -397,6 +409,10 @@ flush_out:
 	}
 	f2fs_update_time(sbi, REQ_TIME);
 out:
+#if defined(CONFIG_UFSTW) && defined(CONFIG_UFSFEATURE30)
+	if (turbo_set)
+		bdev_clear_turbo_write(sbi->sb->s_bdev);
+#endif
 	trace_f2fs_sync_file_exit(inode, cp_reason, datasync, ret);
 	f2fs_trace_ios(NULL, 1);
 	trace_android_fs_fsync_end(inode, start, end - start);
